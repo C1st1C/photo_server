b6a19018861ab67d0fa3d983afa523cc3e8ed41e
R0Mi <roman.michalik@fel.uniza.sk>
2021-03-11 11:04:56 +0100

    Loop change, img open

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import socket
+from PIL import Image
 
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-server.bind(('', 1002)) 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 1002)) 
+print("Waiting for connection!") 
 server.listen()
-
 client_socket, client_address = server.accept()
-
+print("Connected! Hello: " + str(client_address)) 
 file = open('server_image.jpg', "wb")
-image_chunk = client_socket.recv(2048)
-
-while image_chunk:
-    file.write(image_chunk)
-    image_chunk = client_socket.recv(2048)
+    
+while 1:
+    image_chunk = client_socket.recv(1024)
+    if image_chunk == bytes(0): break
+    file.write(image_chunk)  
 
 file.close()
+print("Img saved!")
+#im = Image.open("server_image.jpg")  
+#im.show()
+
+
 client_socket.close()
+print("Connection closed, working on img.")
+
+#
